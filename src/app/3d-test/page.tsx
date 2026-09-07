ffb7e1e999caeaae4c1cc53d2079e8230f02c662
esooo201833 <122326428+esooo201833@users.noreply.github.com>
2026-09-07 05:13:25 +0300

    Remove legacy test pages

--- src/app/3d-test/page.tsx
+++
@@ -1,6 +0,0 @@
-import { MotorcycleScene } from "@/components/three/MotorcycleScene";
-
-export default function ThreeDTestPage() {
-return ( <main className="h-screen w-full bg-[#f4f5f1]"> <MotorcycleScene /> </main>
-);
-}
